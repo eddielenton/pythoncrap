a9bbbbc0cf685e001f9dd07c9668f61d0fa85fb3
eddie <eddielenton@gmail.com>
2022-01-13 22:32:40 +0000

    65

--- interface_configurator.py
+++ interface_configurator.py
@@ -5,26 +5,32 @@ from netmiko import ConnectHandler
 import csv
 from csv import reader
 
-# open file in read mode
+# open csv file
 with open('input_data.csv', 'r') as read_obj:
-    # pass the file object to reader() to get the reader object
     csv_reader = reader(read_obj)
-    # Get all rows of csv from csv_reader object as list of tuples
+    # Create a list of tuples from the rows in the csv file
     migration_list = list(map(tuple, csv_reader))
 
-
 for data in migration_list:
+    #populate vars from each set of tuples
     old_switch = data[1]
     old_interface = data[2]
     new_switch = data[3]
     new_interface = data[4]
-    net_connect = ConnectHandler(device_type='cisco_nxos', host='sandbox-nxos-1.cisco.com', port=22, username='admin', password='Admin_1234!') 
+    # Connection details for the old switch
+    net_connect = ConnectHandler(device_type='cisco_nxos', host=old_switch, port=22, username='admin', password='Admin_1234!') 
+    # Prepare config to run on the old switch
     get_config_command = "show run int " + old_interface + " | grep ^\s"
+    # Send config to run on the old switch
     old_int_config = net_connect.send_command(get_config_command)
-    net_connect2 = ConnectHandler(device_type='cisco_nxos', host='sandbox-nxos-1.cisco.com', port=22, username='admin', password='Admin_1234!') 
+    # Connection details for the new switch
+    net_connect2 = ConnectHandler(device_type='cisco_nxos', host=new_switch, port=22, username='admin', password='Admin_1234!') 
+    # Prepare config to run on the old switch 
     set_config_command = "Interface " + new_interface + "\n" + old_int_config
+    # Convert multiline string to list
     list = []
     for line in set_config_command.split("\n"):
         list.append(line.lstrip())
+    # Send config to run on the new switch
     new_int_config = net_connect2.send_config_set(list)
     print(new_int_config)
