8d24dfba199cbc0a058624fa1fbcfdd6b3e90ce2
eddie <eddielenton@gmail.com>
2022-01-13 21:16:50 +0000

    g

--- interface_configurator.py
+++ interface_configurator.py
@@ -5,15 +5,6 @@ from netmiko import ConnectHandler
 import csv
 from csv import reader
 
-nxosv9000 = {
-    'device_type': 'cisco_nxos',
-    'host':   'sandbox-nxos-1.cisco.com',
-    'username': 'admin',
-    'password': 'Admin_1234!',
-    'port' : 22,
-    'secret': 'Admin_1234!',
-}
-
 # open file in read mode
 with open('input_data.csv', 'r') as read_obj:
     # pass the file object to reader() to get the reader object
@@ -27,9 +18,26 @@ for data in migration_list:
     old_interface = data[2]
     new_switch = data[3]
     new_interface = data[4]
+    old_switch_creds = {
+    'device_type': 'cisco_nxos',
+    'host':   old_switch,
+    'username': 'admin',
+    'password': 'Admin_1234!',
+    'port' : 22,
+    'secret': 'Admin_1234!',
+    }
+    new_switch_creds = {
+    'device_type': 'cisco_nxos',
+    'host':   new_switch,
+    'username': 'admin',
+    'password': 'Admin_1234!',
+    'port' : 22,
+    'secret': 'Admin_1234!',
+    }
     net_connect = ConnectHandler(**old_switch)
-    command = "show run int " + old_interface
-    get_int_config = net_connect.send_command(command)
-    trimmed_config = get_int_config[202:]
+    get_config_command = "show run int " + old_interface + " | grep ^\s"
+    old_int_config = net_connect.send_command(get_config_command)
+
     net_connect = ConnectHandler(**new_switch)
-    get_int_config = net_connect.send_command(trimmed_config)
+    set_config_command = "conf t\n" + old_int_config
+    new_int_config = net_connect.send_command(set_config_command)
