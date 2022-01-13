816f5c0131db7dec200a57024328033f178bd346
eddielenton <eddielenton+github@gmail.com>
2022-01-13 17:47:52 +0000

    Update interface_configurator.py

--- interface_configurator.py
+++ interface_configurator.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function, unicode_literals
 from netmiko import Netmiko
-from getpass import getpass
 import csv
-
-#device_list = ("")
-#old_switch = ("")
-#old_interface = ("")
-#new_switch = ("")
-#new_interface = ("")
-
+from csv import reader
 
 # open file in read mode
 with open('input_data.csv', 'r') as read_obj:
@@ -18,14 +11,13 @@ with open('input_data.csv', 'r') as read_obj:
     # Get all rows of csv from csv_reader object as list of tuples
     migration_list = list(map(tuple, csv_reader))
 
-device_list = ("")
-old_switch = ("")
-old_interface = ("")
-new_switch = ("")
-new_interface = ("")
 
-
-for device in device_list:
+for data in migration_list:
+    old_switch = device[0]
+    old_interface = device[1]
     command = device.pop("command")
     net_connect = Netmiko(**device)
     output = net_connect.send_command(command)
+
+print(old_switch)
+print(old_interface)
